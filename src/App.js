9b14701ed59a2a7960195f80a514f9483907fb5f
ColtonTO <colton1105@gmail.com>
2023-04-06 14:33:43 -0700

    added basic index, basic header/navbar links. basic css. basic index/app to start connecting components

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Header from "./components/Header";
+
+function App() {
+  const personalDetails = {
+    name: "Colton Taylor-Ortiz",
+    location: "Bellingham, WA",
+    email: "colton1105@gmail.com",
+    availability: "Open for work",
+    brand:
+      "With my logical and step-by-step approach to solving problems, I have a deep connection to the projects I work on and strive to meet all expectations - and then some!",
+  };
+
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
